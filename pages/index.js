790e7e2bae9c977ed7c1e778b330fbeb8514d0ff
kekrake <119007880+kekrake@users.noreply.github.com>
2023-02-19 17:12:01 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>KEAKproject</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Bienvenue" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          "nous revenons bientot, on a besoin d'une pause" <code>pages/index.js</code>
         </p>
       </main>
 
